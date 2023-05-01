aa9ab71f372b5c1ed8d9de109f39deab0fc2e2a3
Luis Perrone <perronef5@users.noreply.github.com>
2023-05-01 00:08:51 -0500

    Assume veHNT is already delegated

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -427,8 +427,7 @@ const RewardCalculator = () => {
     if (!emission) return;
     subDAOEmissions = Number(emission.emissionsPerEpoch.replace(/,/g, ""));
     const estimatedReward =
-      (Number(yourVeHNTWithMult) / (Number(yourVeHNTWithMult) + currentVeHNT)) *
-      subDAOEmissions;
+      (Number(yourVeHNTWithMult) / currentVeHNT) * subDAOEmissions;
     setEstimatedRewards(estimatedReward);
     setCalculated(true);
     setLoading(false);
